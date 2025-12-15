49c3bd266995774fd3f450ad7dd7e12b331405ce
Siridhamma-Chamlertwat <s6804062612030@email.kmutnb.ac.th>
2025-12-15 10:49:43 +0700

    Add files via upload

---
+++ Lab362.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main()
+{
+	int score;
+	scanf("%d", &score);
+	
+	if (score<0){	
+	printf("error score");}
+	else if (score>100){
+		printf("error score");}
+	else if (score >= 68){
+	if (score >= 85){
+	    printf("A");}
+	else if (score >= 75){
+	    printf("B");}
+	else printf("C");
+	     printf("\nNeed %d more", 75-score);}
+	else {
+	if (score >= 55)
+    printf("D");
+    else printf("F");
+	}
+	
+	return 0;
+
+		
+	
+}
